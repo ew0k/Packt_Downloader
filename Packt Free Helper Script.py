f901eb52b9289b73392a94993d327e7b6aad196c
ew0k <jakebrown188@gmail.com>
2020-01-10 02:10:20 -0500

    Fix logic error in rename_files

--- Packt Free Helper Script.py
+++ Packt Free Helper Script.py
@@ -16,55 +16,48 @@ def grab_name(download_path):
         for file in files:
             tmp_file_name = os.path.splitext(file)[0]
             if tmp_file_name != file_name and file_name != "":
-                logger("ERROR: probably have more than one book in the tmp download folder")
-                exit(1)
+                logger("ERROR", "probably have more than one book in the tmp download folder")
+                exit_code(1)
             else:
                 file_name = tmp_file_name
+    file_name = file_name.replace("_", " ")
     return file_name
 
 
 def erase_folder_contents(folder):
-    logger("Erasing all files in: " + str(folder))
+    logger("WARNING", "Erasing all files in " + str(folder))
     for curr_dir, dirs, files in os.walk(folder):
         for file in files:
-            logger("Erasing: " + file)
+            logger("WARNING", "Erasing " + file)
             os.remove(folder / file)
 
 
 def rename_files(folder, name):
     for curr_dir, dirs, files in os.walk(folder):
         for file in files:
-            file_name = os.path.splitext(file)[0]
             file_name_extension = os.path.splitext(file)[1]
-            if "_" in file_name:
-                logger("Replacing underscore(s) in " + file_name)
-                file_name = file_name.replace("_", " ")
-                logger("Old file_name: " + str(folder / file))
-                logger("New file_name: " + str(folder/file_name) + file_name_extension)
-                os.rename(folder / file, str(folder/file_name) + file_name_extension)
+            old_name = str(file)
+
             if file_name_extension == ".zip":
-                os.rename(folder / file, str(folder / name) + " Code Files" + file_name_extension)
+                new_name = name + " Code Files" + file_name_extension
+            else:
+                new_name = name + file_name_extension
+
+            logger("WARNING", "Renaming File")
+            logger("INFO", "Old file name: " + old_name)
+            logger("INFO", "New file name: " + new_name)
+            os.rename(folder / old_name, folder / new_name)
 
 
-def move_files(download_path, final_path, name):
-    """
-    1) copy all files in tmp_path into final_path with the new name
-    2) append "Code Files" to .zip file
-    3) check to make sure the book isn't already there and erase tmp folder if it is
-    """
-    
+def move_files(download_path, final_path, name):    
     # Check to see is the book is already there
     book_folder = final_path / name
     if book_folder.exists():
-        logger("ERROR: \"" + name + "\" is already in the Tech Books folder")
+        logger("WARNING", " \"" + name + "\" is already in the Tech Books folder")
         erase_folder_contents(download_path)
-        logger("EXITING")
-        exit(1)
+        exit_code(1)
     else:
         os.mkdir(book_folder)
-
-    # Rename all files in the downloads directory
-    rename_files(download_path, name)
 
     # Move Files
     for curr_dir, dirs, files in os.walk(download_path):
@@ -72,35 +65,52 @@ def move_files(download_path, final_path, name):
             shutil.move(download_path / file, book_folder / file)
 
 
-def logger(message):
-    log_file_path = Path("C:/Users/Jake/OneDrive - rit.edu/Documents/Tech Books/Packt Daily Download Script Files/run-scrips-log.txt")
+def exit_code(exit_number):
+    logger("EXITING", "")
+    exit(exit_code)
+
+
+def logger(log_type, message):
+    log_file_path = Path("C:/Users/Jake/OneDrive - rit.edu/Documents/Tech Books/Packt Daily Download Script Files/Packt Downloader.log")
     with open(log_file_path, "a") as log_file:
-        log_file.write(datetime.now().strftime("%H:%M:%S") + " " + message + "\n")
+        if (message == "\n"):
+            log_file.write("\n")
+        elif (log_type == "EXITING"):
+            log_file.write(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " [" + log_type + "]")
+        else:    
+            log_file.write(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " - [" + log_type + "] - " + message + "\n")
 
 
 def main ():
-    logger("\n")
-    # Grab Books
+    logger(None, "\n")
+    logger("INFO", "Starting Script")
+
+    # Change Script Directory
     script_dir = Path("C:/Users/Jake/OneDrive - rit.edu/Documents/Tech Books/Packt Daily Download Script Files")
     os.chdir(script_dir)
-    logger("Current directory: " + os.getcwd())
-    cmd = "packt-cli --grabd --status_mail >> \"C:/Users/Jake/OneDrive - rit.edu/Documents/Tech Books/Packt Daily Download Script Files/run-scrips-log.txt\" 2> \"C:/Users/Jake/OneDrive - rit.edu/Documents/Tech Books/Packt Daily Download Script Files/run-scrips-error-log.txt\""
-    logger("Running this cmd: " + cmd)
-    # os.system(cmd)
-    # logger("PATH: " + os.environ['PATH'])
-    subprocess.call(cmd, shell=True)
-    logger("Finished packt-cli")
 
-    logger("Starting my code")
+    # Run packt-cli
+    cmd = "packt-cli --grabd --status_mail 2> \"C:/Users/Jake/OneDrive - rit.edu/Documents/Tech Books/Packt Daily Download Script Files/Packt Downloader Errors.log\""
+    logger("INFO", "Running the following command: " + cmd)
+    subprocess.call(cmd, shell=True)
+    logger("INFO", "Finished packt-cli")
+
+    logger("INFO", "Starting my code")
+
     # Set Variables
     download_path = Path(PACKT_BOOKS_DOWNLOAD_PATH)
     final_path = Path(PACKT_BOOKS_FINAL_PATH)
 
-    # Move Files
+    # Get File Name
     file_name = grab_name(download_path)
-    file_name = file_name.replace("_", " ")
+
+    # Rename Files
+    rename_files(download_path, file_name)
+
+    # Move Files to Desired Directory
     move_files(download_path, final_path, file_name)
-    logger("Finished my code")
+
+    logger("INFO", "Finished my code")
 
 
 main()
